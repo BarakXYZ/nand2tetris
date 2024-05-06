ef8bd4f90f4c1803b98a25002613aec630ffd718
barakxyz <ibarakxyz@gmail.com>
2024-05-07 00:27:06 +0300

    Assignment 4 workworkwork

--- nand2tetris/projects/4/fill/Fill.asm
+++ nand2tetris/projects/4/fill/Fill.asm
@@ -20,29 +20,121 @@
 
 @SCREEN
 D=A
-@screen_address
+@screen_address  // 16,384
+M=D  // Store screen address
+
+@1
+D=A
+@offset
 M=D
 
-@LOOP_WHITE  // if 0, white
-D;JEQ
+@32
+D=A
+@row
+M=D
 
-@LOOP_BLACK  // else, black
-0;JMP
+@256
+D=A
+@n
+M=D  // Store number of rows (256)
 
-(LOOP_WHITE)
+@i
+M=1  // Initialize i to 1
+
 @KBD
 D=M
-@LOOP_WHITE  // if 0, white
+@WHITE_LOOP  // if 0 (no keyboard input), white screen
 D;JEQ
 
-@LOOP_BLACK  // else, black
+@BLACK_LOOP  // else, black screen
 0;JMP
 
-(LOOP_BLACK)
+(WHITE_LOOP)  // Make screen white; main body
+// body
+@screen_address
+A=M
+M=0  // Make screen row white
+
+@i  // 1
+D=M
+@n  // 256
+D=D-M  // -511
+@RESET_PARAMS_ENTER_IDLE_WHITE
+D;JEQ  // if 0, jump to idle
+// Else; increase address by 64
+@offset
+D=M
+@screen_address
+M=D+M
+@i
+M=M+1
+@WHITE_LOOP
+0;JMP
+
+(RESET_PARAMS_ENTER_IDLE_WHITE)
+// Reset n (256) & i (1)
+@SCREEN
+D=A
+@screen_address
+M=D
+@256
+D=A
+@n
+M=D
+@i
+M=1
+
+(WHITE_IDLE)
+// Check KBD (post-white screen)
 @KBD
 D=M
-@LOOP_WHITE  // if 0, white
+@WHITE_IDLE  // if 0 (no keyboard input), white screen
 D;JEQ
 
-@LOOP_BLACK  // else, black
+@BLACK_LOOP  // else, black screen
+0;JMP
+
+(BLACK_LOOP)  // Make screen white; main body
+// body
+@screen_address
+A=M
+M=-1  // Make screen row white
+
+@i  // 1
+D=M
+@n  // 256
+D=D-M  // -511
+@RESET_PARAMS_ENTER_IDLE_BLACK
+D;JEQ  // if 0, jump to idle
+// Else; increase address by 64
+@offset
+D=M
+@screen_address
+M=D+M
+@i
+M=M+1
+@BLACK_LOOP
+0;JMP
+
+(RESET_PARAMS_ENTER_IDLE_BLACK)
+// Reset n (256) & i (1)
+@SCREEN
+D=A
+@screen_address
+M=D
+@256
+D=A
+@n
+M=D
+@i
+M=1
+
+(BLACK_IDLE)
+// Check KBD (post-white screen)
+@KBD
+D=M
+@WHITE_LOOP  // if 0 (no keyboard input), white screen
+D;JEQ
+
+@BLACK_IDLE
 0;JMP
