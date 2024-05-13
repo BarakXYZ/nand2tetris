6e0f5a24c7ea9ffbf8561f649f56ceda52186d0e
barakxyz <ibarakxyz@gmail.com>
2024-05-13 22:55:25 +0300

    Unit 5 Assignment workworkworkkkk

---
+++ playground/Assembly.asm
@@ -0,0 +1,45 @@
+// Flush & init
+@R1
+M=0
+
+@sum
+M=0
+@i
+M=1
+
+@R0
+D=M
+@n
+M=D
+
+(MAIN)
+@n
+D=M
+@END
+D;JEQ  // if (n == 0) END, else continue
+
+@i
+D=M
+@sum  // sum+i
+M=M+D
+
+@i  // i++
+M=M+1
+@n
+M=M-1  // n-1
+@MAIN
+0;JMP
+
+// Store value at Register 1
+(END)
+@sum
+D=M
+@R1
+M=D
+
+// Loop4Ever
+(INF)
+@INF
+0;JMP
+
+
