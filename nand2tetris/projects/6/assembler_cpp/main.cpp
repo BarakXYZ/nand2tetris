df451bd7cc961ad5a55bc7787c09ed71f0206834
barakxyz <ibarakxyz@gmail.com>
2024-05-29 15:37:07 +0300

    Assembler Progress - Getting there <3

--- nand2tetris/projects/6/assembler_cpp/main.cpp
+++ nand2tetris/projects/6/assembler_cpp/main.cpp
@@ -1,28 +1,36 @@
 #include <array>
+#include <bitset>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <unordered_map>
-#include <bitset>
 
 class SymbolTable {
   public:
     // Add a symbol to the table with its associated address
-    static void add_symbol(const std::string symbol, int address)
+    static void add_symbol(const std::string symbol, size_t address)
     {
         table[symbol] = address;
     }
 
     // Retrieve the address associated with a symbol
-    // Returns -1 if the symbol is not found
-    static int get_address(const std::string &symbol)
+    // Adds the symbol and return an address if it wasn't found
+    static size_t unique_symbols_counter;
+    static size_t parse_variable(const std::string &symbol, bool debug = 1)
     {
         auto it = table.find(symbol);
         if (it != table.end()) {
+            if (debug)
+                std::cout << "Parsed recognized variable '" << symbol << "', returning address: " << it->second << '\n';
             return it->second;
         }
         else {
-            return -1;
+            std::cout << "Parsed unrecognized variable '" << symbol
+                      << "' assigning & returning address: " << SymbolTable::unique_symbols_counter << '\n';
+            add_symbol(symbol, SymbolTable::unique_symbols_counter);
+            ++SymbolTable::unique_symbols_counter;
+            return parse_variable(symbol, 0);
         }
     }
 
@@ -32,64 +40,100 @@ class SymbolTable {
         return table.find(symbol) != table.end();
     }
 
-    static std::unordered_map<std::string, int> table;
+    static std::unordered_map<std::string, size_t> table;
 };
 
-std::unordered_map<std::string, int> SymbolTable::table = {
-    {"R0", 0}, {"R1", 1}, {"R2", 2}, {"R3", 3}, {"R4", 4}, {"R5", 5}, {"R6", 6}, {"R7", 7},
-    {"R8", 8}, {"R9", 9}, {"R10", 10}, {"R11", 11}, {"R12", 12}, {"R13", 13}, {"R14", 14}, {"R15", 15},
-    {"SCREEN", 16384}, {"KBD", 24576}, {"SP", 0}, {"LCL", 1}, {"ARG", 2}, {"THIS", 3}, {"THAT", 4}
-};
+std::size_t SymbolTable::unique_symbols_counter{16};
+std::unordered_map<std::string, size_t> SymbolTable::table = {
+    {"R0", 0},         {"R1", 1},      {"R2", 2},   {"R3", 3},   {"R4", 4},   {"R5", 5},   {"R6", 6},   {"R7", 7},
+    {"R8", 8},         {"R9", 9},      {"R10", 10}, {"R11", 11}, {"R12", 12}, {"R13", 13}, {"R14", 14}, {"R15", 15},
+    {"SCREEN", 16384}, {"KBD", 24576}, {"SP", 0},   {"LCL", 1},  {"ARG", 2},  {"THIS", 3}, {"THAT", 4}};
 
 class Parser {
   public:
-    void parse_file(std::ifstream &file)
+    // First-Pass: Add the label symbols (i.e. (xxx))
+    static bool parse_first_pass(std::ifstream &file)
     {
-        size_t line_number{1};
+
+        size_t line_number{0};
         std::string line;
-        // alt
-        std::string::size_type start{0};
-        
-        std::size_t var_count{16};
 
-        // First-Pass: Add the label symbols (i.e. (xxx))
         while (std::getline(file >> std::ws, line)) {
-            if (line.front() == '(' && line.at(line.size() - 2) == ')') {
+            // Potentially we can maybe optimize with deleting those comment lines for the second pass
+            if (line.front() == '/') {
+                std::cout << "Comment line\n";
+                continue;
+            }
+
+            if (line.front() == '(') {
+                auto end_label_pos = line.find(')');
                 std::cout << line_number << ": " << line << '\n';
 
                 // Extract label's content
-                std::string content = line.substr(1, line.size() - 3);
+                std::string content = line.substr(1, end_label_pos - 1);
                 std::cout << "Label Content: " << content << '\n';
                 SymbolTable::add_symbol(content, line_number);
+                continue;
             }
             ++line_number;
         }
+        file.clear();
+        file.seekg(0, std::ios::beg);
+        if (!file) {
+            std::cerr << "Failed to reset file position after first pass." << std::endl;
+            return false;
+        }
+        return true;
+    }
+    static bool parse_second_pass(std::ifstream &file, std::stringstream &parsed_file)
+    {
 
-        line_number = 1;
-        // Second-Pass: Add the var. symbols
+        size_t line_number{0};
+        std::string line{};
+
         while (std::getline(file >> std::ws, line)) {
-            if (line.front() == '(' && line.at(line.size() - 2) == ')') {
-                std::cout << line_number << ": " << line << '\n';
+            if (line.front() == '/') {
+                std::cout << "Comment line\n";
+                continue;
+            }
+            // std::cout << line << '\n';
 
-                // Extract label's content
-                std::string content = line.substr(1, line.size() - 3);
-                std::cout << "Label Content: " << content << '\n';
-                SymbolTable::add_symbol(content, line_number);
+            // if A-instruction
+            // if Address (convert to binary)
+            // if Variable (look it up, if it exists, assign the address, if not, allocate new address starting with 16)
+            if (line.front() == '@') {
+                // std::cout << "A-instruction: " << line << '\n';
+                line = line.erase(0, 1);
+                if (line.find('\n')) {
+                    line.pop_back();
+                    // std::cout << "End line removed\n";
+                }
+                size_t variable_address{SymbolTable::parse_variable(line)};
+                parsed_file << variable_address << '\n';
+            }
+            else if (line.front() == '(')
+                continue;
+
+            // if C-instruction (convert to the symbolic underlying fields)
+            else {
+                std::cout << "C-instruction: " << line << '\n';
             }
             ++line_number;
         }
-
+        return true;
     }
 };
 
 class Code {
-public:
-    static std::string decimal_to_binary(int n) {
+  public:
+    static std::string decimal_to_binary(int n)
+    {
         return std::bitset<16>(n).to_string();
     }
 };
 
-int main() {
+int main()
+{
     std::ifstream infile;
     std::string infile_name{"test_files/Max.asm"};
 
@@ -99,8 +143,16 @@ int main() {
         return 1;
     }
 
-    Parser parser;
-    parser.parse_file(infile);
+    if (!Parser::parse_first_pass(infile)) {
+        std::cerr << "First pass parsing failed." << std::endl;
+        return 1;
+    }
+
+    std::stringstream parsed_file{};
+    if (!Parser::parse_second_pass(infile, parsed_file)) {
+        std::cerr << "Second pass parsing failed." << std::endl;
+        return 1;
+    }
 
     // Close the input file after parsing
     infile.close();
@@ -117,7 +169,8 @@ int main() {
 
     // Write the contents of SymbolTable::table to the output file
     for (const auto &column : SymbolTable::table) {
-        outfile << column.first << ' ' << Code::decimal_to_binary(column.second) << '\n';
+        // outfile << column.first << ' ' << Code::decimal_to_binary(column.second) << '\n';
+        outfile << column.first << ' ' << column.second << '\n';
     }
 
     // Close the output file
