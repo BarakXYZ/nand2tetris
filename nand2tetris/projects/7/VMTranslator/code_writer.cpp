a17d0624b2d25544ffb4fb6c230a193385bf3f12
barakxyz <ibarakxyz@gmail.com>
2024-06-09 01:49:45 +0300

    Project 7 Progressss +-80%

--- nand2tetris/projects/7/VMTranslator/code_writer.cpp
+++ nand2tetris/projects/7/VMTranslator/code_writer.cpp
@@ -1,14 +1,16 @@
 #include "code_writer.h"
+#include "utilities.h"
 
-CodeWriter::CodeWriter(std::unique_ptr<std::ofstream> outFile) 
-: outFile(std::move(outFile)){};
+CodeWriter::CodeWriter(std::unique_ptr<std::ofstream> outFile, std::string_view programName) 
+: outFile(std::move(outFile)), programName{programName} {};
 
-auto CodeWriter::writePushConstant(std::string_view segment, int i) -> void {
+auto CodeWriter::writePushConstant(int i) -> void {
     *outFile << "// addr = segmentPointer + i\n";
-    *outFile << "@" << i << '\n' << vmCodeToAssembly::pushConstant;
+    *outFile << "@" << i << '\n';
+    *outFile << vmCodeToAssembly::pushConstant;
 }
 
-auto CodeWriter::writePushSegment(std::string_view segment, int i) -> void {
+auto CodeWriter::writePushPopSegment(vmCommand cmd, std::string_view segment, int i) -> void {
     if (segment == "local")
         *outFile << "@LCL\n";
     else if (segment == "argument")
@@ -17,30 +19,69 @@ auto CodeWriter::writePushSegment(std::string_view segment, int i) -> void {
         *outFile << "@THIS\n";
     else
         *outFile << "@THAT\n";
-    *outFile << "D=A\n" << "@" << i << vmCodeToAssembly::pushSegment;
+
+    *outFile
+        << "D=A\n"
+        << '@' << i << '\n';
+
+    if(cmd == vmCommand::C_PUSH)
+        *outFile << vmCodeToAssembly::pushSegment;
+    else
+        *outFile << vmCodeToAssembly::popSegment;
 }
 
-auto CodeWriter::writePush(std::string_view arg1, std::string_view arg2) -> void {
+auto CodeWriter::writePushPopStatic(vmCommand cmd, int i) -> void {
+    if(cmd == vmCommand::C_PUSH) {
+        *outFile
+            << '@' << programName << i << '\n'
+            << vmCodeToAssembly::pushStatic;
+    } else {
+        *outFile 
+            << vmCodeToAssembly::popStatic
+            << "// *fileName.i = *SP\n"  // Debug
+            << '@' << programName << i << '\n'
+            << "A=M\n"
+            << "M=D\n";
+    }
+}
+auto CodeWriter::writePushPopTemp(vmCommand cmd, int i) -> void {
+    if(cmd == vmCommand::C_PUSH) {
+        *outFile 
+            << "// addr = 5 + i\n" // Debug
+            << '@' << i << '\n'
+            << vmCodeToAssembly::pushTemp;
+    } else {
+        *outFile 
+            << "// addr = 5 + i\n" // Debug
+            << '@' << i << '\n'
+            << vmCodeToAssembly::popTemp;
+    }
+}
+auto CodeWriter::writePushPopPointer(vmCommand cmd, int i) -> void {
+    *outFile << "// *SP = THIS/THAT\n"; // Debug
+    if(!i)
+        *outFile << "@3\n";  // 0 -> THIS
+    else
+        *outFile << "@4\n";  // 1 -> THAT
+    *outFile << vmCodeToAssembly::pushPointer;
+}
+
+auto CodeWriter::writePushPop(vmCommand commandType, std::string_view arg1, std::string_view arg2) -> void {
     // Add comment line for debugging:
     *outFile << "// push " << arg1 << ' ' << arg2 << '\n'; 
     int i{stringViewToInt(arg2)};
 
     // Switch on Memory Segment
-    if (arg1 == "constant") {
-        writePushConstant(arg1, i);
-    }
-    else if (arg1 == "static") {
-
-    }
-    else if (arg1 == "pointer") {
-
-    }
-    else if (arg1 == "temp") {
-
-    }
-    else {  // local, argument, this or that.
-        writePushSegment(arg1, i);
-    }
+    if (arg1 == "constant") 
+        writePushConstant(i);
+    else if (arg1 == "static") 
+        writePushPopStatic(commandType, i);
+    else if (arg1 == "pointer") 
+        writePushPopPointer(commandType, i);
+    else if (arg1 == "temp") 
+        writePushPopTemp(commandType, i);
+    else   // local, argument, this or that.
+        writePushPopSegment(commandType, arg1, i);
 }
 
 auto CodeWriter::writePop(std::string_view arg1, std::string_view arg2) -> void {
--- nand2tetris/projects/7/VMTranslator/utilities.h
+++ nand2tetris/projects/7/VMTranslator/utilities.h
@@ -5,12 +5,154 @@
 #include <charconv>
 #include <iostream>
 
-enum class vmCommand {
+enum vmCommand {
     C_ARITHMETIC,
     C_PUSH, C_POP,
     C_LABEL, C_GOTO,
     C_IF, C_FUNCTION,
     C_RETURN, C_CALL,
+};
+
+struct vmCodeToAssembly {
+    // push constant i == *SP = i, SP++
+    static constexpr std::string_view pushConstant = 
+        // "// *SP = i\n"
+        // "@i\n"
+        "D=A\n"
+        "@SP\n"
+        "A=M\n"
+
+        "// SP++\n"
+        "M=D\n"
+        "M=M+1\n";
+
+    // addr = segmentPointer + i, *SP = *addr, SP++
+    // local (LCL) / argument (ARG) / this (THIS) / that (THAT)
+
+    static constexpr std::string_view pushSegment =
+        // addr = segmentPointer + i\n
+        // @segmentPointer
+        // D=A
+        // @i
+        "A=D+A\n"
+        // "D=A+D\n"
+        // "@R13\n"
+        // "M=D\n"
+
+        "// *SP = *addr\n"
+        // "A=M\n"
+        "D=M\n"
+        "@SP\n"
+        "A=M\n"
+        "M=D\n"
+
+        "// SP++\n"
+        "@SP\n"
+        "M=M+1\n";
+
+    // addr = segmentPointer + i, SP--, *addr = *SP
+    static constexpr std::string_view popSegment =
+        // addr = segmentPointer + i\n
+        // @segmentPointer
+        // D=A
+        // @i
+        "D=D+A\n"
+        "@R13\n"
+        "M=D\n"
+
+        "// SP--\n"  // Debug
+        "@SP\n"
+        "M=M-1\n"
+
+        "// *addr = *SP\n"  // Debug
+        "A=M\n"
+        "D=M\n"
+        "@R13\n"
+        "A=M\n"
+        "M=D\n";
+
+
+    // *SP = *fileName.i, SP++
+    static constexpr std::string_view pushStatic =
+        // @fileName.i
+        // D=M
+        "@SP\n"
+        "A=M\n"
+        "M=D\n"
+
+        "// SP++\n"  // Debug
+        "@SP\n"
+        "M=M+1\n";
+    
+    // SP--, *fileName.i = *SP
+    static constexpr std::string_view popStatic =
+        // SP--
+        "@SP\n"
+        "M=M-1\n"
+        "A=M\n"
+        "D=M\n"
+        // *fileName.i = *SP
+        // @fileName.i
+        // A=M
+        // M=D
+        ;
+
+    // addr = 5 + i, *SP = *addr, SP++
+    static constexpr std::string_view pushTemp =
+        // "// addr = 5 + i\n" // Debug
+        // @i
+        "D=A\n"
+        "@5\n"
+        "A=D+A\n"
+        "D=M\n"
+
+        "// *SP = *addr\n" // Debug
+        "@SP\n"
+        "A=M\n"
+        "M=D\n"
+
+        "// SP++\n" // Debug
+        "@SP\n"
+        "M=M+1\n";
+
+    // addr = 5 + i, SP--, *addr = *SP
+    static constexpr std::string_view popTemp =
+        
+        // addr = 5 + i
+        // @i
+        "D=A\n"
+        "@5\n"
+        "D=D+A\n"
+        "@13\n"
+        "M=D\n"
+
+        // SP--
+        "@SP\n"
+        "M=M-1\n"
+        
+        // *addr = *SP
+        "A=M\n"
+        "D=M\n"
+        "@13\n"
+        "A=M\n"
+        "M=D\n";
+
+
+    // *SP = THIS/THAT, SP++
+    static constexpr std::string_view pushPointer =
+        // "// *SP = THIS/THAT\n" // Debug
+        // if 0
+        // "@3\n"
+        // else
+        // "@4\n"
+        "D=M\n"
+        "@SP\n"
+        "A=M\n"
+        "M=D\n"
+
+        "// SP++\n" // Debug
+        "@SP\n"
+        "M=M+1\n";
 };
 
 int stringViewToInt(std::string_view str);
