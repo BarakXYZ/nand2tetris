2d804af4978d1ce3854625f0fa459b07dba982f1
barakxyz <ibarakxyz@gmail.com>
2024-08-02 19:38:02 +0300

    Project 9: 1. Progress on the final challenge, finishing soon! 2. Some tweaks on dialogue and code. 3. Minor bug fixes

---
+++ nand2tetris/projects/9/testAssembly/testChallenge5.asm
@@ -0,0 +1,13 @@
+    // INIT
+    @NAND
+    M=-1
+
+
+    @NAND
+    D=M  // D=-1
+    @2
+    D=D+A  // D=1
+    @TETRIS
+    D=D+M
+    @WEEK9
+    M=D
---
+++ nand2tetris/projects/9/testAssembly/testChallengeTETRIS.asm
@@ -0,0 +1,19 @@
+    // SymName = TETRIS
+    // Address = 475 (ASCII value of TETRIS haha idk:()
+    // Value = ?
+    // RAM[574]=24
+    // RAM[24]=457
+
+    // RAM[SHIMON]=42
+    // RAM[GNIRUT]=18
+    // RAM[LOLO]=0
+    // RAM[NAND]=-1
+    
+    @574
+    D=A|M  // 0000 0010 0011 1110 | 
+    (HERE)
+    @GO
+    (GO)
+    @HERE
+
+
