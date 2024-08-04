ea7e4631030255dc11454138bf032811353094f1
barakxyz <ibarakxyz@gmail.com>
2024-08-04 16:11:54 +0300

    Project 9: Walkthrough, Arranged code to folders, etc. For some reason when the dialogue sligthly changes, sometimes it might cause weird problems after finishing the game (I really don't know why). I've fixed it by changing the dialogue a bit (literally adding a few blank characters

--- nand2tetris/projects/9/Assembly Solutions/SolutionChallenge3.asm
+++ nand2tetris/projects/9/Assembly Solutions/SolutionChallenge3.asm
@@ -1,12 +1,15 @@
+    //** Written & Designed by BarakXYZ (Barak Arosh Surian) in 2024
+    // As part of Week 9 assignment @NAND2TETRIS by Shimon Shocken & Noam Nisan *//
+
     // Challenge 3 (SHIMON)
 
-    // init
+    // INIT Predefined Value
     @18
     D=A
     @357
     M=D  // RAM[357] = 18 (GNIRUT challenge result)
     
-    // Actual Code:
+    // Actual Challenge:
     @357
     D=M  // D=18
     @666
--- nand2tetris/projects/9/Assembly Solutions/SolutionChallengeLOLO.asm
+++ nand2tetris/projects/9/Assembly Solutions/SolutionChallengeLOLO.asm
@@ -1,8 +1,12 @@
-    // Challenge LOLO (Part of Challenge 4)
+    //** Written & Designed by BarakXYZ (Barak Arosh Surian) in 2024
+    // As part of Week 9 assignment @NAND2TETRIS by Shimon Shocken & Noam Nisan *//
 
-    @1010  // init
+    // Challenge LOLO (Part of Challenge 4)
+    // RAM[LOLO] - Address 1010
+
+    @1010  // @LOLO
     D=A+1  // 1010+1 -> D=1011
-    @LOLI  // @16
+    @LOLI
     M=D-1  // 1011-1 RAM[LOLI] = 1010
 
     (LOLI)
@@ -13,7 +17,7 @@
     @LOLI  // @16
     D+1;JEQ  // Run1: -1 + 1 = 0;JEQ (TRUE) | Run2: 0 + 1 = 1;JEQ (FALSE)
 
-    @1010
+    @1010  // @LOLO
     M=D  // RAM[1010] = 0
 
     // Answer: RAM[1010] = 0
